8e25effe031b407f7aad5a92286b5b10ee1f14ee
Firdous Ali <firdous.ali.tp@gmail.com>
2023-11-03 19:18:37 +0500

    just formatting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import Inputs from "./components/Inputs";
 import Header from "./components/Header";
 
-
 function App() {
-
   return (
     <div className="bg-[url('https://images.pexels.com/photos/912364/pexels-photo-912364.jpeg?cs=srgb&dl=pexels-brett-sayles-912364.jpg&fm=jpg')] flex flex-col md:h-screen w-screen bg-cover bg-center  ">
       <Header />
       <Inputs />
-
     </div>
-
   );
 }
 
